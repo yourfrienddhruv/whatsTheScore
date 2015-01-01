c368c5b54d8ad26ee790a6cf4613037bfe50da45
Nikunj-rank <rank.nikunj93@gmail.com>
2015-01-01 19:29:23 +0530

    First implementation of cricket match result record score and display.

--- whatsTheScore.js
+++ whatsTheScore.js
@@ -1,23 +1,49 @@
+CricketMatchResults = new Mongo.Collection("cricketMatchResults");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+    Meteor.subscribe("cricketMatchResults");
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+    Template.cricketMatchResults.helpers({
+        cricketMatchResults: function () {
+            console.log(CricketMatchResults.find({}).count());
+            return CricketMatchResults.find({}, {sort: {matchDate: -1}});
+        }
+    });
+
+    Template.recordScoreOfCricketMatch.events({
+        "submit .recordScoreOfCricketMatchForm": function (event) {
+            Meteor.call("recordScoreOfCricketMatch",
+                event.target.team1.value,
+                event.target.team2.value,
+                event.target.resultText.value);
+
+            // Clear form
+            event.target.team1.value =
+                event.target.team2.value =
+                    event.target.resultText.value = '';
+
+            // Prevent default form submit
+            return false;
+        }
+    });
+
+
 }
 
+Meteor.methods({
+    recordScoreOfCricketMatch: function (team1, team2, resultText) {
+        CricketMatchResults.insert({team1: team1, team2: team2, resultText: resultText, matchDate: new Date()});
+    }
+});
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        //CricketMatchResults.insert({team1: "India", team2: "Australia", resultText: "Australia won by 9 wickets", matchDate: new Date()});
+    });
+
+    Meteor.publish("cricketMatchResults", function () {
+        return CricketMatchResults.find();
+    });
+
 }
